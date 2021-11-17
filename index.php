187965ff8bccde0223277c072062f822a800a0ec
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-17 18:36:27 +0300

    Попытка подключения к базе данных, не удачно

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 /*Веб-интерфейс просмотра всех доступных экземпляров данного элемента курса
 */
 require('../../config.php');
+//require (__DIR__.'/../../../config.php'); Выдает ошибку JS
 echo 'Веб-интерфейс просмотра всех доступных экземпляров данного элемента';
+/*$viewDb = $DB -> get_recordset ( 'mdl_user' ,  array  $conditions = null ,  $sort = '' ,  $fields = '*' ,  $limitfrom = 0 ,  $limitnum = 0 );
+if ($viewDb->valid()){
+foreach ($viewDb as $record){
+        echo $record;
+}
+} else echo 'Ошибка';
+$viewDb->close();
+*/
