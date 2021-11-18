9e9c7671cc6ef6dc3c8e8cd85b41d72cfad1ad19
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-18 14:30:43 +0300

    Добавление вывода header и footer на форму

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 /*Веб-интерфейс просмотра всех доступных экземпляров данного элемента курса
 */
 require('../../config.php');
+require_once(__DIR__.'/../../config.php');
 //require (__DIR__.'/../../../config.php'); Выдает ошибку JS
 echo 'Веб-интерфейс просмотра всех доступных экземпляров данного элемента';
 /*$viewDb = $DB -> get_recordset ( 'mdl_user' ,  array  $conditions = null ,  $sort = '' ,  $fields = '*' ,  $limitfrom = 0 ,  $limitnum = 0 );
@@ -12,3 +13,12 @@ foreach ($viewDb as $record){
 } else echo 'Ошибка';
 $viewDb->close();
 */
+$title = 'Плагин';
+$PAGE->set_url(new moodle_url('/mod/AnalyticsPlugin/index.php'));
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading($title);
+//echo $controller->render_preview($PAGE);
+echo $OUTPUT->footer();
