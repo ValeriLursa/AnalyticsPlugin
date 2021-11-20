90ad8b394f317d1199ee10b5892d776a96fa058b
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-20 15:55:33 +0300

    Выделение глобальных переменных в отдельный файл

---
+++ classes/global_variables.php
@@ -0,0 +1,16 @@
+<?php
+/*глобальные переменные
+$STUDENT - массив студентов
+$COURSE - массив курсов
+$TEST_COURSE - массив тестов
+$PROGRESS - массив прогрессов
+$SELECT - индекс входных данных
+$SELECT_EDUCATION - инедкс обучения алгоритма (1 - нет старых данных, 2 - есть старые данные)
+*/
+$STUDENT;
+$COURSE;
+$TEST_COURSE;
+$PROGRESS;
+$SELECT = 2;
+$SELECT_EDUCATION = 0;
+?>
