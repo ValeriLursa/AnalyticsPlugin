71abf426504c27ed08d790a782b112d6bf6ceec2
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-20 15:53:30 +0300

    Удаление ненужных полей ввода в форме

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
 <body>
 <h3>Форма ввода данных</h3>
 <form action="controller.php" method="POST">
-    <p>Дата начала курса: <input type="number" name="course[]" /></p>
-    <p></p>
     <p>Дата начала теста 1: <input type="number" name="test_course_start[]" /></p>
     <p>Дата фактического прохождения теста 1: <input type="number" name="test_course_fact[]" /></p>
     <p>Результат теста 1: <input type="number" name="grad_test[]" /></p>
@@ -23,7 +21,6 @@
     <p>Дата фактического прохождения теста 3: <input type="number" name="test_course_fact[]" /></p>
     <p>Результат теста 3: <input type="number" name="grad_test[]" /></p>
     <p>Дата конца теста 3: <input type="number" name="test_course_end[]" /></p>
-    <p>Дата конца курса: <input type="number" name="course[]" /></p>
     <input type="submit" value="Результат по одному курсу">
 </form>
 </body>
