12e6189a9bffc5c5f54d8cfd97e74c8fd7b90b4e
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-21 17:17:06 +0300

    Добавление изменения вывода формы для тестирования или переход к форме для ввода данных пользователя

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-    require "tests\\test_controller_grade_console.php";
-    test_true();
-    test_percent_grade_test1();
-    test_percent_grade_test2();
-    test_percent_grade_test3();
-    test_percent_grade_test4();
-    test_coef_grade_test1();
-    test_coef_grade_test2();
-    test_coef_grade_test3();
-    test_coef_grade_test4();
+    $c = false; //0 - тест, 1 - форма
+    if (!$c)
+    {
+        require "tests\\test_controller_grade_console.php";       
+    }
+    else require "index_form.php";
 ?>
