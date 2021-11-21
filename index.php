89eacdb8617a93e4fa0bfe4f9c82d267afbb213b
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-21 16:29:16 +0300

    Добавление входного файла для модульного тестирования

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Анализатор успеваемости</title>
-<meta charset="utf-8" />
-</head>
-<body>
-<h3>Форма ввода данных</h3>
-<form action="controller.php" method="POST">
-    <p>Дата начала теста 1: <input type="number" name="test_course_start[]" /></p>
-    <p>Дата фактического прохождения теста 1: <input type="number" name="test_course_fact[]" /></p>
-    <p>Результат теста 1: <input type="number" name="grad_test[]" /></p>
-    <p>Дата конца теста 1: <input type="number" name="test_course_end[]" /></p>
-    <p></p>
-    <p>Дата начала теста 2: <input type="number" name="test_course_start[]" /></p>
-    <p>Дата фактического прохождения теста 2: <input type="number" name="test_course_fact[]" /></p>
-    <p>Результат теста 2: <input type="number" name="grad_test[]" /></p>
-    <p>Дата конца теста 2: <input type="number" name="test_course_end[]" /></p>
-    <p></p>
-    <p>Дата начала теста 3: <input type="number" name="test_course_start[]" /></p>
-    <p>Дата фактического прохождения теста 3: <input type="number" name="test_course_fact[]" /></p>
-    <p>Результат теста 3: <input type="number" name="grad_test[]" /></p>
-    <p>Дата конца теста 3: <input type="number" name="test_course_end[]" /></p>
-    <input type="submit" value="Результат по одному курсу">
-</form>
-</body>
-</html>
+<?php
+    require "tests\\test_controller_grade_console.php";
+    test_true();
+?>
