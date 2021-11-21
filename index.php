f929053c00a315f70300c8a6995ee1f359ade019
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-21 16:51:36 +0300

    Вывод резльтатов тестирования

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
     require "tests\\test_controller_grade_console.php";
     test_true();
+    test_percent_grade_test1();
+    test_percent_grade_test2();
+    test_percent_grade_test3();
+    test_percent_grade_test4();
+    test_coef_grade_test1();
+    test_coef_grade_test2();
+    test_coef_grade_test3();
+    test_coef_grade_test4();
 ?>
