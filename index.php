e3e9890901d2a7f9e15340937ce02138f03825c6
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-21 19:11:17 +0300

    Добавление функций для тестирования функций проверки входных данных с формы

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
     $c = false; //0 - тест, 1 - форма
     if (!$c)
     {
-        require "tests\\test_controller_grade_console.php";   
+        //require "tests\\test_controller_grade_console.php";   
         print "<br>";
-        require "tests\\test_controller_dates_console.php";   
+       // require "tests\\test_controller_dates_console.php";
+        print "<br>";
+        require "tests\\test_controller_console.php";    
     }
     else require "index_form.php";
 ?>
