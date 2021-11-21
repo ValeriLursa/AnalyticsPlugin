e19431b3b96160390bcbe1c68f0c9352020a457f
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-21 17:32:29 +0300

    Добавление вывода тестов с датами

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     $c = false; //0 - тест, 1 - форма
     if (!$c)
     {
-        require "tests\\test_controller_grade_console.php";       
+        require "tests\\test_controller_grade_console.php";   
+        print "<br>";
+        require "tests\\test_controller_dates_console.php";   
     }
     else require "index_form.php";
 ?>
