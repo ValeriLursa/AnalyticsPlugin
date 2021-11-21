96486647a304b7fb9783080e5252ff3a70945993
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-21 21:16:14 +0300

    Вывод результата в этот файл

--- view.php
+++ view.php
@@ -1,3 +1,12 @@
 <?php
     require "classes\\controller.php";
+    require "classes\\global_variables.php";
+    echo "Проверка передачи данных на сервер</br>";
+
+    //echo $_POST["test_course_start"][0];
+    //echo switch_date();
+
+    echo "Результат анализа по оценкам: ".alg_grade(0)."</br>";
+    //echo "Результат анализа по датам сдачи: ".alg_dates(0)."</br>";
+    //echo "Результат общего анализа: ".algorithm(0)."</br>";
 ?>
