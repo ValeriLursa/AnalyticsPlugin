13face92e49fd0e56fc2324021bbb386ecc2fd4e
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-23 13:06:24 +0300

    Вывод результатов в виде таблицы

--- view.php
+++ view.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Анализатор успеваемости</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h3>Результат анализа:</h3>
 <?php
     require "classes\\controller.php";
     require "classes\\global_variables.php";
@@ -5,8 +13,32 @@
     if(switch_date())
     echo "Проверка передачи данных на сервер: true</br>"; 
     else echo "Проверка передачи данных на сервер: false</br>";
-
-    echo "Результат анализа по оценкам: ".alg_grade(0)."</br>";
-    echo "Результат анализа по датам сдачи: ".alg_dates(0)."</br>";
-    echo "Результат общего анализа: ".algorithm(0)."</br>";
 ?>
+<table border="1">
+    <tr>
+        <td>
+        Результат анализа по оценкам:
+        </td>
+        <td>
+            <?php echo alg_grade(0); ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        Результат анализа по датам сдачи:
+        </td>
+        <td>
+        <?php echo alg_dates(0); ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        Результат общего анализа:
+        </td>
+        <td>
+        <?php echo algorithm(0); ?>
+        </td>
+    </tr>
+
+</body>
+</html>
