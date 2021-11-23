e21adc0b1c8719b85cdb7a8aa8e9df4b2eabdcf1
Валери Ковалева <kovaleva.valeri71@gmail.com>
2021-11-23 12:32:13 +0300

    Доавблена проверка передачи данных на сервер и вывод результата анализа оценки

--- view.php
+++ view.php
@@ -1,12 +1,12 @@
 <?php
     require "classes\\controller.php";
     require "classes\\global_variables.php";
-    echo "Проверка передачи данных на сервер</br>";
-
-    //echo $_POST["test_course_start"][0];
-    //echo switch_date();
+    
+    if(switch_date())
+    echo "Проверка передачи данных на сервер: true</br>"; 
+    else echo "Проверка передачи данных на сервер: false</br>";
 
     echo "Результат анализа по оценкам: ".alg_grade(0)."</br>";
-    //echo "Результат анализа по датам сдачи: ".alg_dates(0)."</br>";
+    echo "Результат анализа по датам сдачи: ".alg_dates(0)."</br>";
     //echo "Результат общего анализа: ".algorithm(0)."</br>";
 ?>
